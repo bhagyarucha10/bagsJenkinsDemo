4213f9ec7173602ee9565d6f828aea7336453e90
Bhagyashree Chandraiah [UNext] <bhagyashree.chandraiah@u-next.com>
2023-07-04 22:13:47 +0530

    first commit

---
+++ src/com/demo/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class JenkinsDemo {
+int add(int m,int n)
+{
+	return m+n;
+}
+public static void main(String args[])
+{
+	JenkinsDemo d=new JenkinsDemo();
+	System.out.println("The result is"+d.add(2, 3));
+}
+}
